68d2345c51bb95b399a525ad3b97c9d64927cbe1
ticodes <142982604+ticodes@users.noreply.github.com>
2024-01-15 14:59:00 +0300

    Регистрация | БД

---
+++ process_registration.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kursach";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+
+    // Проверка паролей на совпадение
+    if ($password !== $confirm_password) {
+        echo "Пароли не совпадают.";
+        exit();
+    }
+
+    // Хеширование пароля перед сохранением в базу данных
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $hashed_password);
+
+    if ($stmt->execute()) {
+        echo "Регистрация успешна. Данные добавлены в базу данных.";
+        header("Location: index.html");
+        exit();
+    } else {
+        echo "Ошибка при регистрации: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
